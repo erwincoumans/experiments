54f0c68f8ed4efa8642617eeccbc76293c90eb84
Erwin Coumans <erwin.coumans@gmail.com>
2011-08-26 17:46:36 -0700

    add a hint that Cygwin is required

--- build/nacl.sh
+++ build/nacl.sh
@@ -1,4 +1,5 @@
 
+echo Only tested using a Cygwin Bash Shell with make.exe available
 
 export AR=/cygdrive/f/sdks/native_client_sdk_0_5_984/toolchain/win_x86/bin/nacl-ar.exe
 export CC=/cygdrive/f/sdks/native_client_sdk_0_5_984/toolchain/win_x86/bin/nacl-gcc.exe
