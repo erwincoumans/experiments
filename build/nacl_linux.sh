5f38cb1606230b6ef61e0b4930e9497f81d96dbc
Erwin Coumans <erwin.coumans@gmail.com>
2011-10-25 13:37:29 -0700

    add an almost working nacl_linux.sh script (still need to remove the /usr/lib64 from the linker flags)

--- build/nacl_linux.sh
+++ build/nacl_linux.sh
@@ -3,12 +3,13 @@ echo Only tested using a Cygwin Bash Shell with make.exe available
 
 NACL_SDK_ROOT=$(dirname $(readlink -f $0))/nacl/nacl_sdk/pepper_15
 echo $NACL_SDK_ROOT
+./premake4_linux --with-nacl gmake
+
 
 export AR=$NACL_SDK_ROOT/toolchain/linux_x86_newlib/bin/i686-nacl-ar
 export CC=$NACL_SDK_ROOT/toolchain/linux_x86_newlib/bin/i686-nacl-gcc
 export CXX=$NACL_SDK_ROOT/toolchain/linux_x86_newlib/bin/i686-nacl-g++
 
-./premake4_linux --with-nacl gmake
 cd nacl
 export config=release32
 make
