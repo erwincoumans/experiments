339553d84596f39f23a4b205a71c3d6e0acc4efd
Erwin Coumans <erwin.coumans@gmail.com>
2012-05-17 23:37:42 +0200

    add edge-edge ccd test too

--- dynamics/exact-ccd/main.cpp
+++ dynamics/exact-ccd/main.cpp
@@ -9,16 +9,35 @@
 int main(int argc, char* argv[])
 {
 	assert((_controlfp(0,0)&_MCW_RC)==_RC_NEAR);
+	bool result =false;
 
-	Vec3d verts_old[4]={Vec3d(-2,0,1.0000001),Vec3d(0,0,1),Vec3d(-1,0,-1),Vec3d(1,0,-1)};
-	Vec3d verts_new[4]={Vec3d(2.1,0,0.999999),Vec3d(0,0,1),Vec3d(-1,0,-1),Vec3d(1,0,-1)};
+	{
+		//test vertex-triangle
+		Vec3d verts_old[4]={Vec3d(-2,0,1.0000001),Vec3d(0,0,1),Vec3d(-1,0,-1),Vec3d(1,0,-1)};
+		Vec3d verts_new[4]={Vec3d(2.1,0,0.999999),Vec3d(0,0,1),Vec3d(-1,0,-1),Vec3d(1,0,-1)};
 
-	bool is_edge_edge = false;
+		bool is_edge_edge = false;
 
-	rootparity::RootParityCollisionTest test(
-		verts_old[0],verts_old[1],verts_old[2],verts_old[3],
-		verts_new[0],verts_new[1],verts_new[2],verts_new[3],is_edge_edge);
-	
-	bool result = test.run_test();
+		rootparity::RootParityCollisionTest test(
+			verts_old[0],verts_old[1],verts_old[2],verts_old[3],
+			verts_new[0],verts_new[1],verts_new[2],verts_new[3],is_edge_edge);
+
+		result = test.run_test();
+	}
+
+	{
+		//test edge-edge
+		Vec3d verts_old[4]={Vec3d(-1,0,0),Vec3d(1,0,0),Vec3d(0,1,-1),Vec3d(0,1,1)};
+		Vec3d verts_new[4]={Vec3d(-1,0,0),Vec3d(1,0,0),Vec3d(0,0,-1),Vec3d(0,0,1)};
+
+		bool is_edge_edge = true;
+
+		rootparity::RootParityCollisionTest test(
+			verts_old[0],verts_old[1],verts_old[2],verts_old[3],
+			verts_new[0],verts_new[1],verts_new[2],verts_new[3],is_edge_edge);
+
+		result = test.run_test();
+	}
+
 	return 0;
 }
