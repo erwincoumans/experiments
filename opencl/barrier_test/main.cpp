c34c623352a4201df2867559e5ea50949f612c06
erwincoumans <erwin.coumans@gmail.com>
2012-08-01 18:23:19 -0700

    remove duplicate barrier code

--- opencl/barrier_test/main.cpp
+++ opencl/barrier_test/main.cpp
@@ -13,88 +13,8 @@
 #include <iostream>
 #include <assert.h>
 
-#define NUM_THREADS 5
-#define MAX_NESTED_BARRIER_LEVELS 4
+#include "barrier_support.h"
 
-struct Args
-{
-    int m_i[NUM_THREADS];
-    int m_barrierNestingLevel[NUM_THREADS];
-    int m_barrier[MAX_NESTED_BARRIER_LEVELS][NUM_THREADS];
-    int m_savedBarrier[MAX_NESTED_BARRIER_LEVELS][NUM_THREADS];
-
-    Args()
-    {
-        for (int i=0;i<NUM_THREADS;i++)
-        {
-            m_i[i]=0;
-            m_barrierNestingLevel[i] = 0;
-            for (int n=0;n<MAX_NESTED_BARRIER_LEVELS;n++)
-            {
-                m_barrier[n][i]=0;
-                m_savedBarrier[n][i]=0;
-
-            }
-        }
-    }
-};
-
-#define BARRIER(label)     args->m_barrier[args->m_barrierNestingLevel[threadId]][threadId]++; \
-                    return 1; \
-label:\
-
-#define SKIP_TO_BARRIER \
-switch (args->m_barrierNestingLevel[threadId])                                      \
-{                                                                                   \
-case 0:                                                                             \
-    {                                                                               \
-        switch (args->m_barrier[args->m_barrierNestingLevel[threadId]][threadId])   \
-        {                                                                           \
-            case 0:                                                                 \
-                break;                                                              \
-            case 1:                                                                 \
-                goto label01;                                                       \
-            case 2:                                                                 \
-                goto label02;                                                       \
-            case 3:                                                                 \
-                goto label03;                                                       \
-            default:                                                                \
-                printf("error, unknown barrier\n");                                 \
-        };                                                                          \
-        break;                                                                      \
-    }                                                                               \
-case 1:                                                                             \
-    {                                                                               \
-        switch (args->m_barrier[args->m_barrierNestingLevel[threadId]][threadId])   \
-        {                                                                           \
-            case 0:                                                                 \
-                assert(0);                                                          \
-                break;                                                              \
-            case 1:                                                                 \
-                goto label11;                                                       \
-            case 2:                                                                 \
-                goto label12;                                                       \
-            default:                                                                \
-                printf("error, unknown barrier\n");                                 \
-        };                                                                          \
-        break;                                                                      \
-    }                                                                               \
-default:                                                                            \
-    {                                                                               \
-    }                                                                               \
-};
-
-
-#define INC_LEVEL \
-args->m_barrierNestingLevel[threadId]++;\
-args->m_savedBarrier[args->m_barrierNestingLevel[threadId]][threadId] = args->m_barrier[args->m_barrierNestingLevel[threadId]][threadId];
-
-#define DEC_LEVEL \
-args->m_savedBarrier[args->m_barrierNestingLevel[threadId]][threadId] = args->m_barrier[args->m_barrierNestingLevel[threadId]][threadId]; \
-args->m_barrierNestingLevel[threadId]--;
-
-
-#define I (args->m_i[threadId])
 
 int func(Args* args, int threadId)
 {
