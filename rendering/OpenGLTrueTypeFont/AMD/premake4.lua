39d4da74bbcf77654c246f0fc0533b9190a2e554
Erwin Coumans <erwin.coumans@gmail.com>
2012-08-07 17:19:00 -0700

    add rendering\OpenGLTrueTypeFont\AMD\premake4.lua build system file

---
+++ rendering/OpenGLTrueTypeFont/AMD/premake4.lua
@@ -0,0 +1,41 @@
+	
+	hasCL = findOpenCL_AMD()
+	
+	if (hasCL) then
+
+		project "OpenGL_TrueTypeFont_AMD"
+
+		initOpenCL_AMD()
+	
+		language "C++"
+				
+		kind "ConsoleApp"
+		targetdir "../../../bin"
+
+
+		initOpenGL()
+		initGlew()
+
+		includedirs {
+		"../../rendertest",
+		"../../primitives",
+		"../../../bullet2"
+		}
+		
+			
+---		links {"BulletFileLoader"}
+		
+		files {
+			"../main.cpp",
+			"../../rendertest/Win32OpenGLRenderManager.cpp",
+			"../../rendertest/Win32OpenGLRenderManager.h",
+			"../../../bullet2/LinearMath/btAlignedAllocator.cpp",
+			"../../../bullet2/LinearMath/btQuickprof.cpp",
+			"../../../bullet2/LinearMath/btQuickprof.h" ,
+			"../fontstash.cpp",
+                        "../fontstash.h",
+                        "../stb_image_write.h",
+                        "../stb_truetype.h",
+			}
+		
+	end
